f5fbbc757829d5fec3d3dda2839102ca4668fb2b
ZoloLua <zolo79pa@gmail.com>
2025-09-05 20:27:41 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,5 +3,3 @@ Obfuscatored by Zolo Obfuscator
 discord: unknown
 2025 Zolo Obfuscator®
 ]]
-
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/ef65167b82218c4cff25f85bbab26c12"))()
