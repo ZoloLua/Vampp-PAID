9d1b27f6f2155fcfcb7c3aa27f718d1152055fd5
ZoloLua <zolo79pa@gmail.com>
2025-09-05 18:30:57 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,7 @@
+--[[
+Obfuscatored by Zolo Obfuscator
+discord: unknown
+2025 Zolo Obfuscator®
+]]
+
 loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/ef65167b82218c4cff25f85bbab26c12"))()
