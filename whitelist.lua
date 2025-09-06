b1ccc346f10f0f88c9da1d1789f9d5999c2762f1
ZoloLua <zolo79pa@gmail.com>
2025-09-05 21:01:01 -0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,17 +1,1 @@
-return {
--- Zolo
-[1312296921] = true, 
-[8830927584] = true,
--- Dan
-[1408609880] = true,
--- Jay
-[4699959372] = true,
-[5059110081] = true,
--- Darkness
-[4131097566] = true,
-[1522608840] = true,
--- Lazer
-[4729904616] = true,
-[987707321] = true,
--- Lyn
-[1434403408] = true,
+local niggers
